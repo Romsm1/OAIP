16decbef21525aa6d1d1e359b9aa0a4febae8379
Romsm1 <semenkovroman06@gmail.com>
2024-09-25 12:55:45 +0900

    Add files via upload

---
+++ 2СПИ_Семенков_Л1.py
@@ -0,0 +1,192 @@
+# задание 1
+
+def main():
+
+    movie = input("Название фильма: ")
+    cinema = input("Название кинотеатра: ")
+    time = input("Время: ")
+
+    print(f'Билет на "{movie}" в "{cinema}" на {time} забронирован.')
+
+if __name__ == "__main__":
+    main()
+
+
+# задание 2
+
+def main():
+
+    paycheck = float(input("Зарпалата за месяц: "))
+    w_hours = float(input("Кол-во раб часов в выходные: "))
+
+    premiya = paycheck * 0.01 * w_hours
+
+    print(f"Размер премии: {premiya}")
+
+if __name__ == "__main__":
+    main()
+
+
+# задание 3
+
+def main():
+    summa = int(input("Введите сумму: "))
+
+    number_1000 = summa // 1000
+    rem_amount = summa % 1000
+
+    number_100 = rem_amount // 100
+    rem_amount %= 100
+
+    number_10 =rem_amount // 10
+    number_1 = rem_amount % 10
+
+    print(f"{number_1} по 1р")
+    print(f"{number_10} по 10р")
+    print(f"{number_100} по 100р")
+    print(f"{number_1000} по 1000р")
+
+if __name__ == "__main__":
+    main()
+
+
+# задание 4
+
+def main():
+    review = input("Напишите отзыв: ")
+    review = review.lower()
+
+    a = review.find("весело")
+    b = review.find("увлекательно")
+    c = review.find("развлечения")
+
+    print(f"Результат анализа: {a} {b} {c}")
+
+if __name__ == "__main__":
+    main()
+
+
+# заdание 5
+
+def main():
+    a = input()
+    length = len(a)
+
+    if length % 2 !=0:
+        mid_index = length // 2
+        print(a(mid_index))
+    else:
+        mid_index = length // 2 - 1
+        print(a[mid_index])
+
+if __name__ == "__main__":
+    main()
+
+
+# задание 6
+
+def main():
+    feedback = 'Алиса и Вася, большое спасибо за теплый приём!'
+    name1 = feedback[0:5]
+    name2 = feedback[8:12]
+
+    result = name1 + "/" + name2
+    print(f"Назначить премию: {result}")
+
+if __name__ == "__main__":
+    main()
+
+# задание 7
+
+def main():
+    alfavit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    a = int(input("Введите номер буквы: "))
+    index = a - 1
+    for i in range(4):
+        print(alfavit[(index + i) % 26], end=' ')
+
+if __name__ == "__main__":
+    main()
+
+
+# задание 8
+
+new_list = []
+new_list = [1, 2, 3, 4, 5]
+
+new_list.append(6)
+new_list.extend([7, 8])
+
+new_list.remove(2)
+del new_list[0]
+
+s_list = new_list[2:5]
+
+new_list.reverse()
+reversed_list = new_list[::-1]
+
+list_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+element = list_2d[1][2]  # доступ к элементу 2-й строки и 3-го столбца (6)
+list_2d[0][1] = 10
+
+new_list.clear()
+
+
+#задание 9
+
+empty_tuple = ()
+print(empty_tuple)
+
+fulled_tuple = (1, 2, 3, "ошцоуопшцпоупбцущп", 6323.3277779876767)
+print(fulled_tuple)
+
+# задание 10
+
+empty_set = set()
+print(empty_set)
+
+fulled_set = {1, 2, 3, "klwofojf", 802.214}
+print(fulled_set)
+
+empty_set.add(4)
+empty_set.add(5)
+empty_set.add("ABC")
+print(empty_set)
+
+# объединение
+set_a = {1, 2, 3}
+set_b = {3, 4, 5}
+union_set = set_a.union(set_b)
+print(union_set)
+# пересечение
+intersection_set = set_a.intersection(set_b)
+print(intersection_set)
+# разность
+difference_set = set_a.difference(set_b)
+print(difference_set)
+# cимметричная разность
+symmetric_difference_set = set_a.symmetric_difference(set_b)
+print(symmetric_difference_set)
+
+# задание 11
+
+empty_dict = {}
+print(empty_dict)
+
+fulled_dict = {
+    "name": "Roman",
+    "age": 18,
+    "city": "Blagoveshchensk"
+}
+print(fulled_dict)
+
+fulled_dict["job"] = "Student"
+print(fulled_dict)
+
+del fulled_dict["name"]
+print(fulled_dict)
+
+fulled_dict["age"] = "3286"
+print(fulled_dict)
