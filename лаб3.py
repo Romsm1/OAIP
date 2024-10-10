7bd871fe24a535a6ecd1bcc484e45d777bd3f769
Romsm1 <semenkovroman06@gmail.com>
2024-10-10 12:39:24 +0900

    Update лаб3.py

--- лаб3.py
+++ лаб3.py
@@ -1,15 +1,12 @@
 # задание 1
 def main():
-
     while True:
         abc = input("Введите строку: ")
         if abc == " ":
             break
         print(f"Длина строки: {len(abc)}")
 
-
-
-# задание 2
+    # задание 2
 
     negative_numbers = 0
 
@@ -22,9 +19,7 @@ def main():
 
         print(negative_numbers)
 
-
-
-# задание 3
+    # задание 3
 
     num1_max = num2_max = float(input("Введите число: "))
     while True:
@@ -39,11 +34,10 @@ def main():
 
         print(f"Второй максимум = {num2_max}")
 
+    # задание 4
 
-
-# задание 4
-
-    numbers = input("Введите числа через пробел: ").split() # .split нужен для того чтобы считать строку и разбить ее на отдельные элементы
+    numbers = input(
+        "Введите числа через пробел: ").split()  # .split нужен для того чтобы считать строку и разбить ее на отдельные элементы
     min_number = float(numbers[0])
 
     for number in numbers:
@@ -52,14 +46,42 @@ def main():
     print(f"Наименьшее число = {min_number}")
 
 
-
 # задание 5
 
-    
+    number = int(input("Введите число: "))
+
+    while number != 0:
+        if number == 0:
+            break
+        if number % 3 == 0 and number % 7 == 0:
+            print("Караул!")
+            break
+        elif number % 3 == 0:
+            print("Несчастливое")
+        elif number % 7 == 0:
+            print("Опасное")
+        else:
+            print(number)
+        number = int(input("Введите число: "))
 
 
+# задание 6
 
+    summa = 0
+    for a in range(1, 10001):
+        celoe_number = True
+        if a < 2:
+            celoe_number = False
+        else:
+            for i in range(2, int(a**0.5) + 1):
+                if a % i == 0:
+                    celoe_number = False
+                    break
+        if celoe_number:
+            summa += a
+    print(f"Cумма простых числе: {summa}")
 
+# задание 7
 
 if __name__ == "__main__":
     main()
