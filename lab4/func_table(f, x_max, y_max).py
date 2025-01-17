cc05625c81917e9192f4bda20f4c66114bd81118
Romsm1 <semenkovroman06@gmail.com>
2025-01-17 14:04:53 +0900

    Update func_table(f, x_max, y_max).py

--- lab4/func_table(f, x_max, y_max).py
+++ lab4/func_table(f, x_max, y_max).py
@@ -1,2 +1,6 @@
 def func_table(f, x_max, y_max):
-
+    for y in range(y_max + 1):
+        row = []
+        for x in range(x_max + 1):
+            row.append(str(eval(f)))
+        print('\t'.join(row))
