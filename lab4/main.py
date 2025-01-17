f5aac4fa624b793266481e72ec88a55c4ac613ca
Romsm1 <semenkovroman06@gmail.com>
2025-01-17 14:05:09 +0900

    Update main.py

--- lab4/main.py
+++ lab4/main.py
@@ -1,12 +1,12 @@
 from lab4.template import template
+from lab4.func_table import func_table
+from lab4.horse2 import horse2
 
 
 def main():
+    horse2('e4')
     template(5, 4, 5)
-
-
-
-
+    func_table('x ** 2 + y', 3, 5)
 
 
 if __name__ == '__main__':
