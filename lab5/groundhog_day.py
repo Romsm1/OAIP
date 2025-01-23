d56e0f92817d25a695aeb857e66043b808193085
Romsm1 <semenkovroman06@gmail.com>
2025-01-23 09:50:44 +0900

    Update groundhog_day.py

--- lab5/groundhog_day.py
+++ lab5/groundhog_day.py
@@ -1,6 +1,6 @@
 def groundhog_day(strings):
     for i in range(1, len(strings)):
-        difference = [diff for diff in range(len(strings[i])) if strings[i][diff] != strings[i - 1][diff]]
-        if len(difference) > 2:
-            return 1, difference
-        return 0, 0
+        differences = [j for j in range(len(strings[i])) if strings[i][j] != strings[i-1][j]]
+        if len(differences) > 2:
+            return i, differences
+    return 0, 0
