d85e588e532a7617f4aa55ecf9cf64215854e01f
Romsm1 <semenkovroman06@gmail.com>
2025-01-23 10:30:24 +0900

    Update gears.py

--- lab5/gears.py
+++ lab5/gears.py
@@ -1,9 +1,9 @@
 def gears(data, n, m):
-    for box in data:
-        for gear1 in box:
-            for box2 in data:
-                for gear2 in box2:
-                    if gear1 / gear2 == n / m:
-                        return (gear1, gear2)
+    for i, box1 in enumerate(data):
+        for gear1 in box1:
+            for j, box2 in enumerate(data):
+                if i != j:  # Проверяем, чтобы шестерёнки были из разных коробок
+                    for gear2 in box2:
+                        if gear1 / gear2 == n / m:
+                            return (gear1, gear2)
     return (None, None)
-
