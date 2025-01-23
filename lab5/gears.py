c4ccd412f25fd24794a403128056a64fccec5d6a
Romsm1 <semenkovroman06@gmail.com>
2025-01-23 10:26:21 +0900

    Update gears.py

--- lab5/gears.py
+++ lab5/gears.py
@@ -1,7 +1,9 @@
-def gears(gears_list, n, m):
-    all_gears = [gear for sublist in gears_list for gear in sublist]
-    for gear1 in all_gears:
-        for gear2 in all_gears:
-            if gear1 / gear2 == n / m:
-                return gear1, gear2
-    return None, None
+def gears(data, n, m):
+    for box in data:
+        for gear1 in box:
+            for box2 in data:
+                for gear2 in box2:
+                    if gear1 / gear2 == n / m:
+                        return (gear1, gear2)
+    return (None, None)
+
