ce637a285f0e06d3d508d8e2fcd3cd16771bf388
Romsm1 <semenkovroman06@gmail.com>
2025-06-03 18:26:05 +0900

    Update task 4.py

--- lab3 csv/task 4/task 4.py
+++ lab3 csv/task 4/task 4.py
@@ -1,8 +1,6 @@
-# Вводите два числа через пробел, например: 200 60
 print("Введите n (мин. сумма баллов) и m (мин. балл по каждому предмету):")
 n, m = map(int, input().split())
 
-# Теперь вводите строки с данными абитуриентов
 print("Теперь вводите данные абитуриентов, например: Иванов Петр 70 80 90")
 
 with open('exam.csv', 'w', encoding='utf-8') as out_file:
@@ -11,14 +9,17 @@ with open('exam.csv', 'w', encoding='utf-8') as out_file:
     while True:
         try:
             line = input()
+            if not line.strip():
+                break  
+            
             data = line.split()
-
             surname, name = data[0], data[1]
-            scores = list(map(int, data[2:]))
+            scores = list(map(int, data[2:] ))
             total = sum(scores)
 
             if total >= n and all(s >= m for s in scores):
                 result_line = f'{surname};{name};{scores[0]};{scores[1]};{scores[2]};{total}\n'
                 out_file.write(result_line)
-        except:
+
+        except (ValueError, IndexError):
             break
